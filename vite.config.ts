747fb11c38424f2fb69d13d5bf4f82748b0ec14d
hayawar <hayaw@Hayas-MacBook-Air.local>
2024-09-28 09:51:12 +0300

    github workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/bv2/",
   plugins: [react(),svgr(),],
   build: {
     chunkSizeWarningLimit: 1000,
