f9838e12439a13e2d082e181ba2cd903b385ad7f
hayawar <hayaw@Hayas-MacBook-Air.local>
2024-09-28 10:36:26 +0300

    update router for  github workflow

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,7 +2,6 @@ import { createBrowserRouter } from "react-router-dom";
 import Detection from "../core/detection/detection";
 import Reports from "../core/reports/reports";
 import App from "../App";
-import HousingUnits from "../core/housing-units/housingUnits";
 import LandingPage from "../core/landing-page/landing-page";
 
 
