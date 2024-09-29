68d33adfa0ad8a0c41577b47324466c024cba025
hayawar <hayaw@Hayas-MacBook-Air.local>
2024-09-30 01:47:17 +0300

    report-map

--- src/components/button/text-input.tsx
+++
@@ -1,8 +0,0 @@
-import { forwardRef } from 'react';
-
-const TextInput = forwardRef<HTMLInputElement, { placeholder: string }>(
-  (props, ref) => {
-    return <input ref={ref} type="text" placeholder={props.placeholder} />;
-  }
-);
-export default TextInput
