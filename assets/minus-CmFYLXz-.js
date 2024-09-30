073b0aa084b37c1486cfebd6a728a573418eb38c
forwhosesakes <forwhosesakes@users.noreply.github.com>
2024-09-30 04:26:09 +0000

    deploy: 0ecdf52a4d2d1eea31730fe7a588c199e0d2359e

--- assets/minus-CmFYLXz-.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./index-C17M7co4.js";const o=e=>t.createElement("svg",{width:"800px",height:"800px",viewBox:"0 0 24 24",fill:"currentColor",xmlns:"http://www.w3.org/2000/svg",...e},t.createElement("path",{d:"M6 12L18 12",stroke:"currentColor",strokeWidth:2,strokeLinecap:"round",strokeLinejoin:"round"}));export{o as default};
