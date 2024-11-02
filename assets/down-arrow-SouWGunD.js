ddb99bdda553ec722216495290e95d70274afe86
forwhosesakes <forwhosesakes@users.noreply.github.com>
2024-11-02 09:07:34 +0000

    deploy: 5ce1e2bac0cd02460e9f7398c305d384f3c3d35a

--- assets/down-arrow-SouWGunD.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index-BLlm9sPY.js";const r=t=>e.createElement("svg",{xmlns:"http://www.w3.org/2000/svg",width:20,height:20,viewBox:"0 0 20 20",fill:"none",...t},e.createElement("path",{d:"M5 7.5L10 12.5L15 7.5",stroke:"#667085",strokeWidth:1.66667,strokeLinecap:"round",strokeLinejoin:"round"}));export{r as default};
